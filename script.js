50bf494e8cd740dc2a015beb7425a5edb81ba3b7
Mesoma Osuji <macosuji1@sheffield.ac.uk>
2025-12-29 19:10:36 +0000

    added styling (fonts, navbar, title section, watercolour background etc)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Code with SWICS loaded");
+console.log("Hack with SWICS loaded");
+
+function toggleMenu() {
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("responsive");
+}
+
+// automatically close the menu when nav link clicked
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        document.getElementById("navbar").classList.remove("responsive");
+    });
+});
